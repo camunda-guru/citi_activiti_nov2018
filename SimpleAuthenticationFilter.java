0eb25170d54bf06852223c8a4d94abebc62fd57d
eswaribala <Parameswaribala@gmail.com>
2018-11-18 17:00:11 +0530

    Add files via upload

---
+++ SimpleAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.opentext.ecommerce.filters;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class SimpleAuthenticationFilter
+extends UsernamePasswordAuthenticationFilter {
+
+  @Override
+  public Authentication attemptAuthentication(
+    HttpServletRequest request, 
+    HttpServletResponse response) 
+      throws AuthenticationException {
+
+      // ...
+
+      UsernamePasswordAuthenticationToken authRequest
+        = getAuthRequest(request);
+      setDetails(request, authRequest);
+       
+      return this.getAuthenticationManager()
+        .authenticate(authRequest);
+  }
+
+  private UsernamePasswordAuthenticationToken getAuthRequest(
+    HttpServletRequest request) {
+
+      String username = obtainUsername(request);
+      String password = obtainPassword(request);
+      //String domain = obtainDomain(request);
+
+      // ...
+
+      String usernameDomain = String.format("%s%s%s", username.trim(), 
+        String.valueOf(Character.LINE_SEPARATOR), null);
+      return new UsernamePasswordAuthenticationToken(
+        usernameDomain, password);
+  }
+
+  // other methods
+}
